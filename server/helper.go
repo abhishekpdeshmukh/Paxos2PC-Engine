9cc3655a7e26ecf1f835f3b0366a032d0b599774
abhishekpdeshmukh <38159363+abhishekpdeshmukh@users.noreply.github.com>
2024-11-22 11:10:58 -0500

    Changes to Prepare Broadcast

--- server/helper.go
+++ server/helper.go
@@ -11,29 +11,33 @@ func (s *Server) BroadCastPrepare(txn *pb.Transaction) {
 	s.lock.Unlock()
 	s.ballotNum++
 	// var wg sync.WaitGroup
+	fmt.Println("Starting my Paxos with Ballot Number ", s.ballotNum)
 	promiseCount := 0
+	fmt.Println(s.servers)
 	for _, i := range s.servers {
+		if(i!= s.ServerID){
+			fmt.Println(i)
+			go func(n int) {
+				c, ctx, conn := setUpServerServerSender(n)
+				defer conn.Close()
 
-		go func(n int) {
-			c, ctx, conn := setUpServerServerSender(n)
-			defer conn.Close()
+				// Make Prepare request
+				promise, err := c.Prepare(ctx, &pb.PrepareRequest{
+					Ballot: &pb.Ballot{
+						BallotNum: int32(s.ballotNum),
+						NodeID:    int32(s.ServerID),
+					},
+					LogSize: int32(len(s.transactionLog)),
+				})
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(promise)
+					promiseCount++
+				}
 
-			// Make Prepare request
-			promise, err := c.Prepare(ctx, &pb.PrepareRequest{
-				Ballot: &pb.Ballot{
-					BallotNum: int32(s.ballotNum),
-					NodeID:    int32(s.ServerID),
-				},
-				LogSize: int32(len(s.transactionLog)),
-			})
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Println(promise)
-				promiseCount++
-			}
-
-		}(i)
+			}(i)
+		}
 
 	}
 	for promiseCount < (len(s.servers)/2)+1 {
